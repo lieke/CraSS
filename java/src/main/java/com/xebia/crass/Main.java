02225580cec503fc15f6110660f535bc0b3e2dbb
Martin van Vliet <mvanvliet@xebia.com>
2010-06-22 21:10:01 +0200

    Implemented interface to Cassandra

--- java/src/main/java/com/xebia/crass/Main.java
+++ java/src/main/java/com/xebia/crass/Main.java
@@ -3,11 +3,11 @@ package com.xebia.crass;
 import java.io.IOException;
 import java.util.Arrays;
 
-import com.xebia.crass.wordcount.WordCountingTwitterListener;
-
 public class Main {
 	public static void main(String args[]) throws IOException {
-		WordCountingTwitterListener counter = new WordCountingTwitterListener();
+		//WordCountingTwitterListener counter = new WordCountingTwitterListener();
+		CassandraTwitterListener counter = new CassandraTwitterListener("localhost", 9160, "Keyspace1");
+		
 		TwitterStreamConsumer consumer = new TwitterStreamConsumer(Arrays.<TwitterListener>asList(counter));
 		
 		consumer.start();
